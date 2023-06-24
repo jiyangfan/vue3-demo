072f92c42e072fdd74d8e4522f0828a0baf0d8a6
986253007@qq.com <986253007@qq.com>
2023-06-24 23:27:07 +0800

    添加gzip、br压缩 vite-plugin-compression

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    viteCompression({
+      threshold: 10240,
+      // algorithm: 'brotliCompress'
+      // deleteOriginFile: true
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
